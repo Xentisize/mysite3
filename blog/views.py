d81bd0196c82d29f6ab9b247f5cd97e587018718
Xens Chan <xentisize@gmail.com>
2020-06-02 17:43:08 +0800

    P.54 Adding comment form

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -37,7 +37,24 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
-    ctx = {"post": post}
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    ctx = {
+        "post": post,
+        "comments": comments,
+        "new_comment": new_comment,
+        "comment_form": comment_form,
+    }
     return render(request, "blog/post/detail.html", ctx)
 
 
